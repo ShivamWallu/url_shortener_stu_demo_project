c51330dd429cc4570c30156d9b614580dbaee220
Shivam Wallu <141025670+ShivamWallu@users.noreply.github.com>
2024-08-14 15:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, flash
 from pymongo import MongoClient
 import string
 import random
 from dotenv import load_dotenv
 import os
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -11,19 +12,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 # MongoDB connection
-client = MongoClient('mongodb://localhost:27017/')        
+client = MongoClient(os.getenv('MONGO_URI'))
 db = client['url_shortener']
-urls_collection = db['urls']                                                                    
+urls_collection = db['urls']
 
 def generate_short_url():
     characters = string.ascii_letters + string.digits
     short_url = ''.join(random.choice(characters) for _ in range(6))
-    link = urls_collection.find_one({'short_url': short_url})
-
-    if link:
+    if urls_collection.find_one({'short_url': short_url}):
         return generate_short_url()
-    else:
-        return short_url
+    return short_url
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -42,6 +40,7 @@ def index():
         }
         urls_collection.insert_one(new_url)
 
+        flash('URL has been shortened!')
         return render_template('index.html', short_url=short_url)
 
     return render_template('index.html')
@@ -51,27 +50,8 @@ def redirect_to_url(short_url):
     link = urls_collection.find_one({'short_url': short_url})
     if link:
         return redirect(link['original_url'])
-    else:
-        return "URL not found", 404
+    return "URL not found", 404
 
+# Note: The following line is commented out because Vercel or serverless environments handle server running.
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-#https://tokyols.co.in/
-    
-
-#https://scholarships.punjab.gov.in/         
-
-
-
-
